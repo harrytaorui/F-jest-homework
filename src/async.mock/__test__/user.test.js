19d1826b8fb04e476060d8a4545ad8898a31f39f
ruitao <harrytaorui@gmail.com>
2020-11-13 09:07:53 +0800

    [taorui]:完成任务

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,14 +1,23 @@
 import { register } from "../user";
+import { verifyUsername } from "../verify";
 
-jest.mock("../verify");
+jest.mock("../verify", () => ({
+  verifyPassword: jest.fn(() => true),
+  verifyUsername: jest.fn(() => true),
+}));
 jest.mock("axios");
 
+beforeEach(() => {
+  jest.resetModules();
+});
+
 describe("register", () => {
-  test("should post user when validated", () => {
-    // TODO 19: add test here
+  test("should post user when validated", async () => {
+    await expect(register("harry", "123")).resolves.toEqual({});
   });
 
-  test("should reject with Error when username is invalid", () => {
-    // TODO 20: add test here
+  test("should reject with Error when username is invalid", async () => {
+    verifyUsername.mockImplementation(() => false);
+    await expect(register("harry", "123")).rejects.toThrow(Error);
   });
 });
--- src/mock.class/__test__/vaccineTest.test.js
+++ src/mock.class/__test__/vaccineTest.test.js
@@ -2,26 +2,46 @@ import VaccineTest from "../vaccineTest";
 import Recipient from "../recipient";
 import Covid19Vaccine from "../covid19Vaccine";
 
+const mockGetHasAntibodies = jest.fn(() => false);
+const mockAcceptInjection = jest.fn();
+
 jest.mock("../recipient", () => {
   // mock class实现
+  return jest.fn().mockImplementation(() => {
+    return {
+      getHasAntibodies: mockGetHasAntibodies,
+      acceptInjection: mockAcceptInjection,
+    };
+  });
 });
 
 beforeEach(() => {
-  // clear mock here
+  // clear mock
+  Recipient.mockClear();
+  mockGetHasAntibodies.mockReset();
+  mockAcceptInjection.mockClear();
 });
 
 describe("inject", () => {
   test("should recipient accept injection with vaccine", () => {
     // TODO 14: add test here
+    const vaccineTest = new VaccineTest();
+    vaccineTest.inject();
+    expect(mockAcceptInjection).toBeCalledWith(expect.any(Covid19Vaccine));
   });
 });
 
 describe("test", () => {
   test("should get Success if recipient has antibodies", () => {
     // TODO 15: add test here
+    const vaccineTest = new VaccineTest();
+    mockGetHasAntibodies.mockImplementation(() => true);
+    expect(vaccineTest.test()).toBe("Vaccine Test Success");
   });
 
   test("should get Failed if recipient has no antibodies", () => {
     // TODO 16: add test here
+    const vaccineTest = new VaccineTest();
+    expect(vaccineTest.test()).toBe("Vaccine Test Failed");
   });
 });
